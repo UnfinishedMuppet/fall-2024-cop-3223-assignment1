956742f4e1aa0a16f1b721bdbdba1ae419dbab13
Emma Lundquist <em649650@ucf.edu>
2024-09-07 03:54:17 +0000

    Completed calculateDistance(), working on perimeter

--- fracturing.c
+++ fracturing.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
+
 // Asks for 4 variables and uses the distance formula to calculate the distance. 
 double calculateDistance()
 {
@@ -10,20 +11,43 @@ double calculateDistance()
     scanf("%lf %lf", &x1, &x2);
 
     // Asks for and stores y values
-    printf("Enter y1 and y2:");
+    printf("Enter y1 and y2:\n");
     double y1 = 1, y2 = 2;
     scanf("%lf %lf", &y1, &y2);
     
     //Calculates distance
-    double power = 2;
-    double xDist = x1 - x2;
-    double yDist = y1 - y2;
-    double pow(double xDist, double power );
-    double pow(double yDist, double power);
-    double Dist = xDist + yDist;
-    double sqrt(double Dist);
+    double distance = sqrt(pow(x2-x1, 2) + pow(y2-y1,2));
+    printf("%lf\n", distance);
+    return distance;
+
+}
+
+
+double calculatePerimeter()
+{
+    printf("Enter x1 and y1:\n");
+    double x1 = 1, y1 = 1;
+    scanf("%lf %lf", &x1, &x2);
+
+    // Asks for and stores y values
+    printf("Enter x2 and y2:\n");
+    double x2 = 1, y2 = 2;
+    scanf("%lf %lf", &y1, &y2);
+
+
+
+
+
+}
+
+
+
+
+
+
+int main()
+{
+    calculateDistance();
     return 0;
-
-
 
 }
