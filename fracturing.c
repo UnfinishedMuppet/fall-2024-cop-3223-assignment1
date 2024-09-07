3b5fdc193b5c68a302705401995e1ec512ef7fe0
Emma Lundquist <em649650@ucf.edu>
2024-09-07 03:10:28 +0000

    Still working on calculateDistance

--- fracturing.c
+++ fracturing.c
@@ -4,13 +4,26 @@
 // Asks for 4 variables and uses the distance formula to calculate the distance. 
 double calculateDistance()
 {
+    // Asks for and stores x values
     printf("Enter x1 and x2:\n");
     double x1 = 1, x2 = 1;
     scanf("%lf %lf", &x1, &x2);
 
-    printf("Enter y1 and y2:")
+    // Asks for and stores y values
+    printf("Enter y1 and y2:");
     double y1 = 1, y2 = 2;
     scanf("%lf %lf", &y1, &y2);
+    
+    //Calculates distance
+    double power = 2;
+    double xDist = x1 - x2;
+    double yDist = y1 - y2;
+    double pow(double xDist, double power );
+    double pow(double yDist, double power);
+    double Dist = xDist + yDist;
+    double sqrt(double Dist);
+    return 0;
+
 
 
 }
