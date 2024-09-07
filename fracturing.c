caaf5fe7ebc47399c08fc9a003e2752e6cd4c72c
Emma Lundquist <em649650@ucf.edu>
2024-09-07 04:07:11 +0000

    calculatePerimeter() almost done

--- fracturing.c
+++ fracturing.c
@@ -25,14 +25,21 @@ double calculateDistance()
 
 double calculatePerimeter()
 {
+    // Asks for and stores (x1,y1)
     printf("Enter x1 and y1:\n");
     double x1 = 1, y1 = 1;
-    scanf("%lf %lf", &x1, &x2);
+    scanf("%lf %lf", &x1, &y1);
 
-    // Asks for and stores y values
+    // Asks for and stores (x2,y2)
     printf("Enter x2 and y2:\n");
     double x2 = 1, y2 = 2;
-    scanf("%lf %lf", &y1, &y2);
+    scanf("%lf %lf", &x2, &y2);
+
+    // Calculates Perimeter (x-h)^2 + (y-k)^2
+    double radius = sqrt(pow(x1 - x2, 2) + pow(y1-y2,2));
+    double perimeter = 2 * PI * radius
+    printf("%lf",&perimeter);
+    return perimeter;
 
 
 
@@ -47,7 +54,9 @@ double calculatePerimeter()
 
 int main()
 {
+    #define PI = 3.14159
     calculateDistance();
+    calculatePerimeter();
     return 0;
 
 }
