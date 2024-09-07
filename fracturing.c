b96251acd4f5c238204141da331cb69f0fa2d9be
Emma Lundquist <em649650@ucf.edu>
2024-09-07 19:52:18 +0000

    Added in .3 behind every % sign in the print statements.

--- fracturing.c
+++ fracturing.c
@@ -1,8 +1,8 @@
+// Emma Lundquist, em649650
+
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
-// For my SANITY I am putting the terminal output here:           gcc fracturing.c && -lm ./a.out
-// Distance = DIAMETER
 
 // Asks for 4 variables and uses the distance formula to calculate the distance. 
 double calculateDistance()
@@ -19,11 +19,11 @@ double calculateDistance()
     
     //Calculates distance
     double distance = sqrt(pow(x2-x1, 2) + pow(y2-y1,2));
-    printf("Point #1 Entered: x1 =%lf", x1);
-    printf("; y1 =%lf\n", y1);
-    printf("Point #2 Entered: x2 =%lf", x2);
-    printf("; y2 =%lf\n", y2);
-    printf("The distance between two points is %lf\n", distance);
+    printf("Point #1 Entered: x1 =%.3lf", x1);
+    printf("; y1 =%.3lf\n", y1);
+    printf("Point #2 Entered: x2 =%.3lf", x2);
+    printf("; y2 =%.3lf\n", y2);
+    printf("The distance between two points is %.3lf\n", distance);
     return distance;
 
 }
@@ -33,22 +33,23 @@ double calculatePerimeter()
 {
     double radius = calculateDistance();
     double perimeter = 2 * PI * radius;
-    printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
+    printf("The perimeter of the city encompassed by your request is %.3lf\n", perimeter);
     return perimeter;
 
 }
 
+// Uses calculateDistance() to determine area
 double calculateArea()
 {
     double diameter = calculateDistance();
     double radius = diameter/2;
     double area = pow(radius, 2) * PI;
-    printf("The area of the city encompassed by your request is %lf\n", area);
+    printf("The area of the city encompassed by your request is %.3lf\n", area);
     return 2;
 
 }
 
-
+//Asks for 4 variables and uses the distance formula to calculate the width.
 double calculateWidth()
 {
     // Asks for and stores x values
@@ -63,25 +64,45 @@ double calculateWidth()
     
     //Calculates Width
     double width = sqrt(pow(x2-x1, 2) + pow(y2-y1,2));
-    printf("Point #1 Entered: x1 =%lf", x1);
+    printf("Point #1 Entered: x1 =%.3lf", x1);
     printf("; y1 =%lf\n", y1);
-    printf("Point #2 Entered: x2 =%lf", x2);
+    printf("Point #2 Entered: x2 =%.3lf", x2);
     printf("; y2 =%lf\n", y2);
-    printf("The width of the city encompassed by your request is -whatever is computed %lf\n", width);
+    printf("The width of the city encompassed by your request is %.3lf\n", width);
     return 3;
-
-
-
-
 
 }
 
+//Asks for 4 variables and uses the distance formula to calculate the height.
+double calculateHeight()
+{
+    // Asks for and stores x values
+    printf("Enter x1 and x2:\n");
+    double x1 = 1, x2 = 1;
+    scanf("%lf %lf", &x1, &x2);
+
+    // Asks for and stores y values
+    printf("Enter y1 and y2:\n");
+    double y1 = 1, y2 = 2;
+    scanf("%lf %lf", &y1, &y2);
+    
+    //Calculates Height
+    double height = sqrt(pow(x2-x1, 2) + pow(y2-y1,2));
+    printf("Point #1 Entered: x1 =%.3lf", x1);
+    printf("; y1 =%.3lf\n", y1);
+    printf("Point #2 Entered: x2 =%lf", x2);
+    printf("; y2 =%.3lf\n", y2);
+    printf("The height of the city encompassed by your request is %.3lf\n", height);
+    return 1;
+
+}
 
 
 
 // Can confirm as of 1am 9/7 that Distance is WORKING
 // 1:15, Perimeter is WORKING
 // 2:09pm Area is WORKING
+// 3:33pm Width is WORKING
 
 
 
@@ -91,6 +112,7 @@ int main(int argc, char **argv)
     calculatePerimeter();
     calculateArea();
     calculateWidth();
+    calculateHeight();
     return 0;
 
 }
