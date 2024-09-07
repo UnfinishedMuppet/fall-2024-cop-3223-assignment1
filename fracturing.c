f1991e0a9547e408e24a0f58741c47e6324ecf2e
Emma Lundquist <em649650@ucf.edu>
2024-09-07 17:54:43 +0000

    Reworking Perimeter to include calculateDistance()

--- fracturing.c
+++ fracturing.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-#define PI = 3.14159
-
+#define PI 3.14159
+// For my SANITY I am putting the terminal output here:           gcc fracturing.c && -lm ./a.out
 
 // Asks for 4 variables and uses the distance formula to calculate the distance. 
 double calculateDistance()
@@ -24,7 +24,7 @@ double calculateDistance()
 }
 
 
-double calculatePerimeter()
+/*double calculatePerimeter()
 {
     // Asks for and stores (x1,y1)
     printf("Enter x1 and y1:\n");
@@ -39,8 +39,24 @@ double calculatePerimeter()
     // Calculates Perimeter (x-h)^2 + (y-k)^2
     double radius = sqrt(pow(x1 - x2, 2) + pow(y1-y2,2));
     double perimeter = 2 * PI * radius;
-    printf("%lf",&perimeter);
+    printf("%lf", perimeter);
     return perimeter;
+
+}
+
+*/
+
+double calculateArea()
+{
+    double diameter = calculateDistance();
+    double radius = diameter/2;
+    double area = pow(radius, 2) * PI;
+    printf("%lf", area);
+    return 2;
+
+
+
+
 
 
 
@@ -50,13 +66,13 @@ double calculatePerimeter()
 
 
 
-
-
+// Can confirm as of 1am 9/7 that Distance is working, Perimeter is not yet.
+// 1:15, Perimeter is WORKING
 
 int main()
 {
     calculateDistance();
-    calculatePerimeter();
+    //calculatePerimeter();
     return 0;
 
 }
