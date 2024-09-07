7190a3f91a48c7fabdde8be1e86a2b7a50a65da6
Emma Lundquist <em649650@ucf.edu>
2024-09-07 02:55:56 +0000

    Started working on calculate distance formula.

--- Fracture.c
+++
@@ -1,4 +0,0 @@
-int main() {
-    number = 2+2
-    return 0;
-}
---
+++ fracturing.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+// Asks for 4 variables and uses the distance formula to calculate the distance. 
+double calculateDistance()
+{
+    printf("Enter x1 and x2:\n");
+    double x1 = 1, x2 = 1;
+    scanf("%lf %lf", &x1, &x2);
+
+    printf("Enter y1 and y2:")
+    double y1 = 1, y2 = 2;
+    scanf("%lf %lf", &y1, &y2);
+
+
+}
