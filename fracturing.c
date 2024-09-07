ea3bf47854cdb1312ac90ca97953aa650dd9ae87
Emma Lundquist <em649650@ucf.edu>
2024-09-07 19:25:19 +0000

    Changed calculateDistance() to include display of Points 1 and 2

--- fracturing.c
+++ fracturing.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #define PI 3.14159
 // For my SANITY I am putting the terminal output here:           gcc fracturing.c && -lm ./a.out
+// Distance = DIAMETER
 
 // Asks for 4 variables and uses the distance formula to calculate the distance. 
 double calculateDistance()
@@ -18,45 +19,39 @@ double calculateDistance()
     
     //Calculates distance
     double distance = sqrt(pow(x2-x1, 2) + pow(y2-y1,2));
-    printf("%lf\n", distance);
+    printf("Point #1 Entered: x1 =%lf", x1);
+    printf("; y1 =%lf\n", y1);
+    printf("Point #2 Entered: x2 =%lf", x2);
+    printf("; y2 =%lf\n", y2);
+    printf("The distance between two points is %lf\n", distance);
     return distance;
 
 }
 
-
-/*double calculatePerimeter()
+// Uses calculateDistance() to determine perimeter
+double calculatePerimeter()
 {
-    // Asks for and stores (x1,y1)
-    printf("Enter x1 and y1:\n");
-    double x1 = 1, y1 = 1;
-    scanf("%lf %lf", &x1, &y1);
-
-    // Asks for and stores (x2,y2)
-    printf("Enter x2 and y2:\n");
-    double x2 = 1, y2 = 2;
-    scanf("%lf %lf", &x2, &y2);
-
-    // Calculates Perimeter (x-h)^2 + (y-k)^2
-    double radius = sqrt(pow(x1 - x2, 2) + pow(y1-y2,2));
+    double radius = calculateDistance();
     double perimeter = 2 * PI * radius;
-    printf("%lf", perimeter);
+    printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
     return perimeter;
 
 }
-
-*/
 
 double calculateArea()
 {
     double diameter = calculateDistance();
     double radius = diameter/2;
     double area = pow(radius, 2) * PI;
-    printf("%lf", area);
+    printf("The area of the city encompassed by your request is %lf\n", area);
     return 2;
 
+}
 
 
-
+double calculateWidth()
+{
+    
 
 
 
@@ -66,13 +61,18 @@ double calculateArea()
 
 
 
-// Can confirm as of 1am 9/7 that Distance is working, Perimeter is not yet.
-// 1:15, Perimeter is WORKING
 
-int main()
+// Can confirm as of 1am 9/7 that Distance is WORKING
+// 1:15, Perimeter is WORKING
+// 2:09pm Area is WORKING
+
+
+
+int main(int argc, char **argv)
 {
     calculateDistance();
-    //calculatePerimeter();
+    calculatePerimeter();
+    calculateArea();
     return 0;
 
 }
